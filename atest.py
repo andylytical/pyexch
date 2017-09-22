932cac9d22f177b4451faed467d3503916d2a96c
Andy Loftus <aloftus@gmail.com>
2017-09-22 00:01:26 -0500

    Update test to use fake netrc file

--- atest.py
+++ atest.py
@@ -1,10 +1,17 @@
 import pyexch
 import datetime
 import pprint
+import netrc
+import os
+
+netrcfn = os.getenv( 'NETRC' )
+nrc = netrc.netrc( netrcfn )
+( u, a, p ) = nrc.authenticators( 'PYEXCH' )
+nrc_parts = { 'login': u, 'account': a, 'pwd': p }
 
 #ptr_regex = { 'NOTWORK': '(sick|doctor|dr. appt|vacation|OOTO|OOO|out of the office|out of office)' }
 #pyex = pyexch.PyExch( regex_map=ptr_regex )
-pyex = pyexch.PyExch()
+pyex = pyexch.PyExch( **nrc_parts )
 start = datetime.datetime( 2017, 1, 1 )
 #events = pyex.get_events_filtered( start )
 #pprint.pprint( events )
--- runtest.sh
+++ runtest.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-export PYEXCH_USER='aloftus'
-export PYEXCH_AD_DOMAIN='UOFI'
-export PYEXCH_EMAIL_DOMAIN='illinois.edu'
+export NETRC='anetrc'
 export PYEXCH_REGEX_JSON='{ 
 "IAMSICK" : "(sick|doctor|dr. appt)",
 "VACA" : "(vacation|OOTO|OOO|out of the office|out of office)" 
 }'
-export PYEXCH_PWD_FILE=$HOME/.ssh/imap_illinois_edu
 
 python atest.py
