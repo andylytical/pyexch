b7e7d07c504cd72da476fcb850fd14a19051fe42
Andy Loftus <aloftus@gmail.com>
2018-06-16 09:59:41 -0500

    clean up unused code

--- atest.py
+++ atest.py
@@ -1,22 +1,11 @@
 import pyexch
 import datetime
 import pprint
-import netrc
-import os
-
-#netrcfn = os.getenv( 'NETRC' )
-#nrc = netrc.netrc( netrcfn )
-#( u, a, p ) = nrc.authenticators( 'EXCH' )
-#nrc_parts = { 'login': u, 'account': a, 'pwd': p }
-#pyex = pyexch.PyExch( **nrc_parts )
-
-#ptr_regex = { 'NOTWORK': '(sick|doctor|dr. appt|vacation|OOTO|OOO|out of the office|out of office)' }
-#pyex = pyexch.PyExch( regex_map=ptr_regex )
 
 pyex = pyexch.PyExch()
 
 start = datetime.datetime( 2018, 1, 1 )
-#events = pyex.get_events_filtered( start )
-#pprint.pprint( events )
-report = pyex.per_day_report( start )
-pprint.pprint( report )
+events = pyex.get_events_filtered( start )
+pprint.pprint( events )
+#report = pyex.per_day_report( start )
+#pprint.pprint( report )
